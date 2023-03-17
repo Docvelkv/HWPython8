b990f5f6473ec16f9ea671a48207276697bc6d60
Docvel <docvel@yandex.ru>
2023-03-18 02:37:13 +0300

    task 38 is solved

--- task38.py
+++ task38.py
@@ -33,18 +33,65 @@ def new_contact():
 
 def contact_search() -> str:
     """
-    Поиск в файле и вывод в консоль строки, содержащей
-    введённые символы.
+    Поиск в файле и вывод в консоль строки, содержащей введённые символы.
 
     Returns:
         str: Все найденные строки с исходной нумерацией.
     """
     print("Введите данные для поиска: ", end="")
     str_search = input()
+    list_str = []
     with open(book, "r", encoding="utf-8") as file:
         for num, line in enumerate(file, 1):
             if str_search.lower() in line.lower():
-                print(f"{num}. {line.strip()}")
+                list_str.append(f"{num}. {line}")
+    if len(list_str) > 0:
+        for i in list_str:
+            print(i, end="\r")
+    else:
+        print("Контакт не найден.")
+
+
+def changing_contact():
+    """
+    Переписывает выбранную строку новыми данными
+    """
+    all_contacts = []
+    with open(book, "r", encoding="utf-8") as file:
+        for num, line in enumerate(file, 1):
+            print(f"{num}. {line.strip()}")
+            all_contacts.append(line)
+    print("Введите номер контакта для изменения: ", end="")
+    change_num = correct_int()
+    print(all_contacts[change_num - 1])
+    print("Введите новые данные.")
+    print("Введите ФИО: ", end="")
+    full_name = input()
+    print("Введите номер телефона: ", end="")
+    num_tel = input()
+    new_contact = f"{full_name} | {num_tel}"
+    all_contacts.pop(change_num - 1)
+    all_contacts.insert(change_num - 1, new_contact)
+    with open(book, "w", encoding="utf-8") as file:
+        file.writelines(all_contacts)
+    print("Контакт обновлён.")
+
+
+def deleting_contact():
+    """
+    Выводит все строки файла и удаляет строку с выбранным номером.
+    """
+    all_contacts = []
+    with open(book, "r", encoding="utf-8") as file:
+        for num, line in enumerate(file, 1):
+            print(f"{num}. {line.strip()}")
+            all_contacts.append(line)
+        print("Выберите номер строки для удаления: ", end="")
+        del_num = correct_int()
+        all_contacts.pop(del_num - 1)
+    with open(book, "w", encoding="utf-8") as file:
+        file.writelines(all_contacts)
+    print("Контакт удалён.")
 
 
 def menu_phone_book():
@@ -59,7 +106,10 @@ def menu_phone_book():
         print("Выберите режим работы:\n"
               "1 - Прочитать весь справочник.\n"
               "2 - Добавить новый контакт.\n"
-              "3 - Найти контакт.")
+              "3 - Найти контакт.\n"
+              "4 - Изменить контакт.\n"
+              "5 - Удалить контакт.\n"
+              "0 - Выход.")
         act = correct_int()
         if act == 1:
             read_all_contacts()
@@ -69,6 +119,14 @@ def menu_phone_book():
             is_phonebook_mode = False
         elif act == 3:
             contact_search()
+            is_phonebook_mode = False
+        elif act == 4:
+            changing_contact()
+            is_phonebook_mode = False
+        elif act == 5:
+            deleting_contact()
+            is_phonebook_mode = False
+        elif act == 0:
             is_phonebook_mode = False
         else:
             print("Такого режима работы нет. "
